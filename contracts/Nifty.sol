dfdc0d2bbef37e7e70b95992899eb52d08ca18fe
Felix Green <work@felix.green>
2021-08-24 17:03:14 -0400

    add minting function to contract

--- contracts/Nifty.sol
+++ contracts/Nifty.sol
@@ -5,4 +5,21 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Nifty is ERC721 {
     constructor() ERC721("Nifty", "NFT") {}
+
+    uint256 idTracker;
+
+    function mintOne() public {
+        internalMint(msg.sender);
+    }
+
+    function mintMany(uint256 number) public {
+        for (uint256 i; i < number; i++) {
+            internalMint(msg.sender);
+        }
+    }
+
+    function internalMint(address to) internal {
+        _safeMint(to, idTracker);
+        idTracker++;
+    }
 }
