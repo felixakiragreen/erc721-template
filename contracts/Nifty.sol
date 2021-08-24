e1c44ff09616ff131f4d242b630d654e5e628ec6
Felix Green <work@felix.green>
2021-08-24 16:02:03 -0400

    rename

--- contracts/Nifty.sol
+++ contracts/Nifty.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MyNFT is ERC721 {
-    constructor() ERC721("Ethereum", "ETH") {}
+contract Nifty is ERC721 {
+    constructor() ERC721("Nifty", "NFT") {}
 }
